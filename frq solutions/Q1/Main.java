8a028482fc7c73bd918ee081b00737a963a297e1
VasylDorofeev <vasyld2@nycstudents.net>
2024-05-21 11:50:43 -0400

    Create Main.java

---
+++ frq solutions/Q1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("2024 Q1");
+    System.out.println();
+
+    Feeder f1 = new Feeder(2000);
+    System.out.println(f1.simulateManyDays(10,5));
+  }
+}
